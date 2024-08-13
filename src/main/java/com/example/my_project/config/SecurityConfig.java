682b0d2a17ce7c76f5a6b6db5d751dde969b5cf4
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2024-08-12 21:50:27 -0600

    Cambios iniciales, integrando con servidor

--- src/main/java/com/example/my_project/config/SecurityConfig.java
+++ src/main/java/com/example/my_project/config/SecurityConfig.java
@@ -2,7 +2,11 @@ package com.example.my_project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,31 +18,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/login").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/home", true)
-                                .permitAll()
-                )
-                .logout(logout ->
-                        logout
-                                .logoutUrl("/logout") // Asegúrate de que el URL coincida con el del botón en el HTML
-                                .logoutSuccessUrl("/login") // Redirige al login después de cerrar sesión
-                                .invalidateHttpSession(true)
-                                .deleteCookies("JSESSIONID")
-                                .permitAll()
-                );
+        http.authorizeHttpRequests( authHttp ->
+                authHttp
+                        .requestMatchers(HttpMethod.GET, "/authorized").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/home").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+                        .requestMatchers(HttpMethod.GET, "/admin").hasAuthority("SCOPE_write")
+                        .anyRequest().authenticated()
+        )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(login -> login.loginPage("/oauth2/authorization/client-app"))
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
 
-    @Bean
+  /*  @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user")
@@ -47,5 +43,5 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user);
-    }
+    }*/
 }
